1789cae920a8da62b8b6a41f7adac01836f82f22
rajapattanayak <pattanayak.raja@gmail.com>
2019-03-19 17:05:20 +0530

    Initial code for kafka producer and consumer

---
+++ src/main/java/com/demo/controllers/KafkaController.java
@@ -0,0 +1,31 @@
+package com.demo.controllers;
+
+import com.demo.engine.Producer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/kafka")
+public class KafkaController {
+
+    private final Producer producer;
+
+    @Autowired
+    KafkaController(Producer producer) {
+        this.producer = producer;
+    }
+
+    @PostMapping(value = "/publish/text")
+    public void sendMessageToKafkaTopic(@RequestParam("message") String message) {
+        this.producer.sendMessage(message);
+    }
+
+    @PostMapping(value = "/publish/json")
+    public void sendJSONMessageToKafkaTopic(@RequestBody String message) {
+        this.producer.sendMessage(message);
+    }
+}
